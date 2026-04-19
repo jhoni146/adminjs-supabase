33a429ee9a9b3c35135b4c95167d357f137579c7
Jonatan <jhoni146@gmail.com>
2026-04-19 22:32:20 +0200

    ipv4 supabase

--- db.js
+++ db.js
@@ -3,4 +3,7 @@ import { Sequelize } from 'sequelize';
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    family: 4   // 👈 fuerza IPv4
+  }
 });
