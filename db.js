a9a3f324f37bae0e0e3723b160e01a0b34c7f41c
Jonatan <jhoni146@gmail.com>
2026-04-20 13:21:24 +0200

    crear tablas auto

--- db.js
+++ db.js
@@ -7,3 +7,12 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
     family: 4   // 👈 fuerza IPv4
   }
 });
+
+// 🟩 sincronización automática segura
+sequelize.sync({ alter: true })
+  .then(() => {
+    console.log('Base de datos sincronizada automáticamente (alter: true)');
+  })
+  .catch((err) => {
+    console.error('Error al sincronizar la base de datos:', err);
+  });
