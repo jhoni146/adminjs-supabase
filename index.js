a34710e979ac64f4680e0cd1dec9f2b9a23a2eca
Jonatan <jhoni146@gmail.com>
2026-04-25 01:09:28 +0200

    mergeado

--- index.js
+++ index.js
@@ -57,11 +57,7 @@ async function generarMensualidadesAutomaticas() {
       miembroId: m.id,
       reclutaId: null,
       mes: mesActual,
-<<<<<<< HEAD
       cuota: 3.50,
-=======
-      cuota: 3.50, // 👈 cuota por defecto
->>>>>>> 4b8717e9 (asddddddrr)
       pagado: false,
       nota: '',
     });
@@ -152,7 +148,6 @@ const adminJs = new AdminJS({
         ],
 
         properties: {
-<<<<<<< HEAD
           miembroId: {
             reference: 'Miembros',
             isVisible: { list: true, edit: true, show: true, filter: true },
@@ -164,19 +159,6 @@ const adminJs = new AdminJS({
             isVisible: { list: true, edit: true, show: true, filter: true },
             populate: true,
           },
-=======
-        miembroId: {
-          reference: 'Miembros',
-          isVisible: { list: true, edit: true, show: true, filter: true },
-          populate: true,
-        },
-
-        reclutaId: {
-          reference: 'Reclutas',
-          isVisible: { list: true, edit: true, show: true, filter: true },
-          populate: true,
-        },
->>>>>>> 4b8717e9 (asddddddrr)
 
           mes: {
             type: 'string',
@@ -226,13 +208,8 @@ const adminJs = new AdminJS({
         ],
         properties: {
           nombre: {
-<<<<<<< HEAD
             isTitle: true,
           },
-=======
-          isTitle: true,
-        },
->>>>>>> 4b8717e9 (asddddddrr)
           fechaInicio: { type: 'string' },
           cursos: {
             type: 'string',
